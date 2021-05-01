75c6b02de26d1a87b044e656d32cd45fc742cea5
OscarIvanLopez <oscari.lopez@potros.itson.edu.mx>
2021-05-01 15:17:03 -0700

    no important commit

--- src/components/app/Profile.js
+++ src/components/app/Profile.js
@@ -3,43 +3,49 @@ import "./styles/profile.css";
 import profilePic from "../../assets/profile.jpg";
 
 const Profile = () => {
-  return (
-    <div className="container profile-container">
-      <div className="profile">
-        <img src={profilePic} alt="profile pic" className="profile-pic" />
-        <div className="description">
-          <h1>Aydin Berenice</h1>
-          <p>Estoy toda hermosa</p>
-        </div>
+    return (
+        <div className="container profile-container">
+            <div className="profile">
+                <img
+                    src={profilePic}
+                    alt="profile pic"
+                    className="profile-pic"
+                />
+                <div className="description">
+                    <h1>Aydin Berenice</h1>
+                    <p>Estoy toda hermosa</p>
+                    <button className="btn btn-primary btn-follow">
+                        Follow
+                    </button>
+                </div>
 
-        <div className="profile-info">
-          <div className="location">
-            <i class="bi bi-geo-alt-fill"></i>
-            <p>Londres</p>
-          </div>
-          <div className="birthday">
-            <i class="bi bi-calendar-date-fill"></i>
-            <p>05-nov-2001</p>
-          </div>
-          <div className="email">
-            <i class="bi bi-envelope-fill"></i>
-            <p>aydin@hermosa.com</p>
-          </div>
-          <div className="social-contact">
-            <i className="bi bi-twitter"></i>
-            <i className="bi bi-github"></i>
-          </div>
-        </div>
+                <div className="profile-info">
+                    <div className="location">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <p>Londres</p>
+                    </div>
+                    <div className="birthday">
+                        <i class="bi bi-calendar-date-fill"></i>
+                        <p>05-nov-2001</p>
+                    </div>
+                    <div className="email">
+                        <i class="bi bi-envelope-fill"></i>
+                        <p>aydin@hermosa.com</p>
+                    </div>
+                    <div className="social-contact">
+                        <i className="bi bi-twitter"></i>
+                        <i className="bi bi-github"></i>
+                    </div>
+                </div>
 
-        <div className="work">
-          <p>Work</p>
-          <p>Python backend developer</p>
+                <div className="work">
+                    <p>Work</p>
+                    <p>Python backend developer</p>
+                </div>
+            </div>
+            .
         </div>
-      </div>
-
-      .
-    </div>
-  );
+    );
 };
 
 export default Profile;
