dbd5ea46f798ef47dfcaf144977f7ba67140daca
OscarIvanLopez <oscari.lopez@potros.itson.edu.mx>
2021-05-16 22:51:55 -0700

    backup

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -6,76 +6,83 @@ import { useDispatch } from "react-redux";
 import { getUserInfoGithub, startGithubLogin } from "../../actions/authActions";
 
 const Register = () => {
-  
-  const [values, handleInputChange] = useForm();
-  const dispatch = useDispatch();
+    const [values, handleInputChange] = useForm();
+    const [registerValues, handleRegisterInputChange] = useForm({
+        name: "",
+        email: "",
+        password1: "",
+        password2: "",
+    });
+    const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(values);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values);
+    };
 
-  useEffect(() => {
-    dispatch(getUserInfoGithub());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getUserInfoGithub());
+    }, [dispatch]);
 
-  const githubAuth = () => {
-    dispatch(startGithubLogin());
-  };
+    const githubAuth = () => {
+        dispatch(startGithubLogin());
+    };
 
-  return (
-    <div className="register-card">
-      <h1>Welcome to Latibule</h1>
-      <p>Let's write some blogs!</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="name"
-          className="mt-2 m-2"
-          type="text"
-          placeholder="Name..."
-          onChange={handleInputChange}
-        />
-        <input
-          name="email"
-          className="mt-2 m-2"
-          type="text"
-          placeholder="youremail@email.com"
-          onChange={handleInputChange}
-        />
-        <input
-          name="password1"
-          className="mt-2 m-2"
-          type="text"
-          placeholder="Password"
-          onChange={handleInputChange}
-        />
-        <input
-          name="password2"
-          className="mt-2 m-2"
-          type="text"
-          placeholder="Confirm your password"
-          onChange={handleInputChange}
-        />
+    return (
+        <div className="register-card">
+            <h1>Welcome to Latibule</h1>
+            <p>Let's write some blogs!</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="name"
+                    className="mt-2 m-2"
+                    type="text"
+                    placeholder="Name..."
+                    onChange={handleInputChange}
+                />
+                <input
+                    name="email"
+                    className="mt-2 m-2"
+                    type="text"
+                    placeholder="youremail@email.com"
+                    onChange={handleInputChange}
+                />
+                <input
+                    name="password1"
+                    className="mt-2 m-2"
+                    type="text"
+                    placeholder="Password"
+                    onChange={handleInputChange}
+                />
+                <input
+                    name="password2"
+                    className="mt-2 m-2"
+                    type="text"
+                    placeholder="Confirm your password"
+                    onChange={handleInputChange}
+                />
 
-        <button className="btn btn-outline-primary mt-1">Create account</button>
-      </form>
-      <hr />
-      <p>or</p>
+                <button className="btn btn-outline-primary mt-1">
+                    Create account
+                </button>
+            </form>
+            <hr />
+            <p>or</p>
 
-      <div className="sign-up-networks">
-        <button className="btn-github" onClick={githubAuth}>
-          <i className="bi bi-github"></i> Sign up with Github{" "}
-        </button>
-        <button className="btn-twitter">
-          <i className="bi bi-twitter"></i> Sign up with Twitter{" "}
-        </button>
-        <button className="btn-google">
-          <img src={googleLogo} alt="google logo" />
-          Sign up with Google{" "}
-        </button>
-      </div>
-    </div>
-  );
+            <div className="sign-up-networks">
+                <button className="btn-github" onClick={githubAuth}>
+                    <i className="bi bi-github"></i> Sign up with Github{" "}
+                </button>
+                <button className="btn-twitter">
+                    <i className="bi bi-twitter"></i> Sign up with Twitter{" "}
+                </button>
+                <button className="btn-google">
+                    <img src={googleLogo} alt="google logo" />
+                    Sign up with Google{" "}
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Register;
